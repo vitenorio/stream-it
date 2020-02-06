ae21f675c1a98810d6e20e335cfa830e24dd86ef
MichelOliveiraNas <michel.oliveira999@hotmail.com>
2020-02-05 23:23:32 -0300

    Creation of the user model using mongoose

---
+++ Back-end/src/Models/UserModel.js
@@ -0,0 +1,38 @@
+const mongoose = require('mongoose');
+
+const UserSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: true
+    },
+    userName: {
+        type: String,
+        required: true
+    },
+    email: {
+        type: String,
+        required: true
+    },
+    password: {
+        type: String,
+        required: true
+    },
+    phone: {
+        type: String,
+        required: false
+    },
+    dateOfBirth: {
+        type: Date,
+        required: true
+    },
+    githubUrl: {
+        type: String,
+        required: false
+    },
+    jobPosition: {
+        type: String,
+        required: false
+    }
+})
+
+mongoose.model('Users', UserSchema);
