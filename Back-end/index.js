d5c72c2339cdb17d136ebc4809325621947a19da
MichelOliveiraNas <michel.oliveira999@hotmail.com>
2020-02-10 22:06:48 -0300

    creation of the user registration method and listing of users and their routes

--- Back-end/index.js
+++ Back-end/index.js
@@ -1,9 +1,19 @@
 const express = require('express');
+const mongoose = require('mongoose');
+const routes = require('./src/routes');
+const requireDir = require('require-dir');
 
 const app = express();
 
-app.get('/', (req, res) => {
-    res.send({message: "Conexão estabelecida!"})
+mongoose.connect('mongodb+srv://DevMichel:michel999@cluster0-e6zxv.mongodb.net/StreamIt?authSource=admin&replicaSet=Cluster0-shard-0&readPreference=primary&appname=MongoDB%20Compass&ssl=true', {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
 });
+
+requireDir('./src/Models/');
+
+//Permitindo que o express "entenda" o formato JSON.
+app.use(express.json());
+app.use(routes);
 
 app.listen(3030);
