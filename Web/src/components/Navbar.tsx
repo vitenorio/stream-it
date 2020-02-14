1361884e35fdfad67d0ec5b2cfc7eac70af2bb6b
Vitoria <vitoria.outsmart@gmail.com>
2020-02-14 16:55:19 -0300

    add home and navbar initial

---
+++ Web/src/components/Navbar.tsx
@@ -0,0 +1,9 @@
+import * as React from 'react'
+import styled from 'styled-components'
+
+
+const NavbarContainer = styled.div``
+
+const Navbar: React.FC = () => {
+    return false as any
+}
---
+++ Web/src/screens/Home/HomeScreen.tsx
@@ -0,0 +1,5 @@
+import * as React from 'react'
+
+const HomeScreen: React.FC = () => {
+    return false as any
+}
