27c91da39cb38252c0c2ca25c276b1c2bd8c43fe
Vitoria <vitoria.outsmart@gmail.com>
2020-02-18 12:58:29 -0300

    Revert "add home and navbar initial"
    This reverts commit 1361884e35fdfad67d0ec5b2cfc7eac70af2bb6b.

--- Web/src/components/Navbar.tsx
+++
@@ -1,9 +0,0 @@
-import * as React from 'react'
-import styled from 'styled-components'
-
-
-const NavbarContainer = styled.div``
-
-const Navbar: React.FC = () => {
-    return false as any
-}
--- Web/src/screens/Home/HomeScreen.tsx
+++
@@ -1,5 +0,0 @@
-import * as React from 'react'
-
-const HomeScreen: React.FC = () => {
-    return false as any
-}
