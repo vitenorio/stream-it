67f8aadfa8f02e5c0cab3b8ca85acafeba8c8890
MichelOliveiraNas <michel.oliveira999@hotmail.com>
2020-06-14 21:44:23 -0300

    Making icons appear on the tab bar

--- Mobile/src/navigation.tsx
+++ Mobile/src/navigation.tsx
@@ -1,45 +1,41 @@
 import React from 'react'
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs'
-import { Feather } from '@expo/vector-icons';
+import { Feather } from '@expo/vector-icons'
 
 import Home from './screens/Home'
 import Profile from './screens/Profile'
-
-const icons: any = {
-    Home: {
-        lib: Feather,
-        name: 'home'
-    },
-    Profile: {
-        lib: Feather,
-        name: 'user'
-    }
-}
 
 const Tab = createBottomTabNavigator()
 
 export default function Navigation() {
     return (
         <Tab.Navigator
-            screenOptions={({ route }) => ({
-                tabBarIcon: ({ color, size }) => {
-                    const { lib: Icon, name } = icons[route.name]
-                    return <Icon name={name} size={size} color={color} />
+            tabBarOptions={{
+                inactiveTintColor: '#C4C4C4',
+                activeTintColor: '#E2A9A3',
+                tabStyle: {
+                    backgroundColor: '#403E5A',
                 }
-            })}
+            }}
         >
             <Tab.Screen
                 name="Home"
                 component={Home}
                 options={{
                     title: "Início",
+                    tabBarIcon: () => (
+                        <Feather name="home" size={24} color="#C4C4C4" />
+                    )
                 }}
             />
             <Tab.Screen
                 name="Perfil"
                 component={Profile}
                 options={{
-                    title: "Perfil"
+                    title: "Perfil",
+                    tabBarIcon: () => (
+                        <Feather name="user" size={24} color="#C4C4C4" />
+                    )
                 }}
             />
         </Tab.Navigator>
