093cc3ccdef5638434e057e8f4ad45e86a6b318b
vitenorio <vitoriabasto17@gmail.com>
2020-07-20 00:10:17 -0300

    Revert "Merge pull request #20 from vitenorio/revert-16-feat/signUpWebScreen"
    This reverts commit 9e5363a5479750e49eddd00bdf66ef2b870b7876, reversing
    changes made to 5f2653c6b87cd6b821ea4398b868c0b0523f0c86.

--- Web/src/router/routes.ts
+++ Web/src/router/routes.ts
@@ -1,5 +1,6 @@
 import App from "../App"
 import { LoginScreen } from "../screens/Login/LoginScreen"
+import { SignUpScreen } from "../screens/SignUp/SignUpScreen"
 
 export const unauthRoutes = [
   {
@@ -15,5 +16,10 @@ export const allRoutes = [
     key: 1,
     path: "/",
     component: App
+  },
+  {
+    key: 2,
+    path: "/SignUp",
+    component: SignUpScreen
   },
 ]
