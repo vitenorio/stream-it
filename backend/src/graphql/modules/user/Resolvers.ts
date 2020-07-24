0739baa0354772af330291ece0d0a7f9a0f3d974
vitenorio <vitoriabasto17@gmail.com>
2020-07-23 23:47:46 -0300

    add create user mutation

--- backend/src/graphql/modules/user/Resolvers.ts
+++ backend/src/graphql/modules/user/Resolvers.ts
@@ -5,6 +5,14 @@ export default {
         users: () => conn('users').select('*')
     },
     Mutation: {
-        
+        createUser: async (root: any, args: any) => {
+    
+            const { name, email, password } = args.input
+            const user = { name, email, password }
+     
+            await conn('users').insert(user)
+
+            return user    
+        },
     }
 }
