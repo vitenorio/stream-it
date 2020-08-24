e18455483764084eb4fc420b93744630a2a880db
MichelOliveiraNas <michel.oliveira999@hotmail.com>
2020-08-23 22:17:35 -0300

    Graphql configuration in the mobile version

--- Mobile/App.tsx
+++ Mobile/App.tsx
@@ -1,4 +1,8 @@
 import React from 'react'
+import { ApolloProvider } from 'react-apollo'
+
+import client from './src/apolloClient'
+
 import { StatusBar } from 'react-native'
 import { NavigationContainer } from '@react-navigation/native'
 
@@ -8,10 +12,10 @@ import Approach from './src/screens/Approach'
 
 export default function App() {
   return (
-    <>
+    <ApolloProvider client={client}>
       <StatusBar barStyle="light-content" />
       <Approach />
-    </>
+    </ApolloProvider>
     // <NavigationContainer>
     //   <Navigation />
     // </NavigationContainer>
